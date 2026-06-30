173ea7ad3aec8c458f64f63e9d087dea869040d8
mcyasar <mcyasar@gmail.com>
2026-06-30 15:26:26 +0300

    updated resilience pipeline order

--- LinkShortener.Infrastructure/Resilience/RedisResiliencePolicy.cs
+++ LinkShortener.Infrastructure/Resilience/RedisResiliencePolicy.cs
@@ -11,20 +11,20 @@ internal static class ResilienceSetup
     public static IServiceCollection AddResilienceStrategy(this IServiceCollection services)
     {
         var defaultResiliencePipeline = new ResiliencePipelineBuilder()
-            // 1. KATMAN: TIMEOUT (Zaman Aşımı Koruması)
-            // Komut 2000 ms içinde dönmezsa Polly işlemi zorla iptal eder (Hata fırlatır)
-            .AddTimeout(TimeSpan.FromMilliseconds(500))
-
             // 1. STRATEJİ: Genel Yeniden Deneme (Retry)
             .AddRetry(new RetryStrategyOptions
-            {                
-                ShouldHandle = new PredicateBuilder().Handle<TimeoutRejectedException>().Handle<Exception>(), 
+            {
+                ShouldHandle = new PredicateBuilder().Handle<TimeoutRejectedException>().Handle<Exception>(),
                 MaxRetryAttempts = 3,
                 Delay = TimeSpan.FromMilliseconds(200),
                 BackoffType = DelayBackoffType.Exponential // 200ms, 400ms, 800ms olarak esne
             })
 
-            // 2. KATMAN: CIRCUIT BREAKER (Devre Kesici)
+            // 2. KATMAN: TIMEOUT (Zaman Aşımı Koruması)
+            // Komut 2000 ms içinde dönmezsa Polly işlemi zorla iptal eder (Hata fırlatır)
+            .AddTimeout(TimeSpan.FromMilliseconds(500))
+
+            // 3. KATMAN: CIRCUIT BREAKER (Devre Kesici)
             .AddCircuitBreaker(new CircuitBreakerStrategyOptions
             {
                 FailureRatio = 0.5, // İsteklerin %50'si hata verirse devreyi aç
